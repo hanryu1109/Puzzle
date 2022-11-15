392ab43311057aa1c1a51b8ba5ad78a598f3c67a
Areum Han <hanryu1109@gmail.com>
2022-11-15 21:08:22 +0900

    feat: 라우트 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-function App() {
-  return <div className="App">SLIDER GAME</div>;
-}
+import { Routes, Route } from "react-router-dom";
 
+import Answer from "./views/Answer";
+import Game from "./views/Game";
+import NotFound from "./views/NotFound";
+import Main from "./views/Main";
+import Menu from "./views/Menu";
+
+const App = () => (
+  <Routes>
+    <Route index element={<Main />} />
+    <Route path="/menu" element={<Menu />} />
+    <Route path="/menu/:menu_id/game" element={<Game />} />
+    <Route path="/menu/:menu_id/answer" element={<Answer />} />
+    <Route path="*" element={<NotFound />} />
+  </Routes>
+);
 export default App;
