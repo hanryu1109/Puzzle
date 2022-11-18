16b22e77c4ed5721a3c24634a5bc4f8e6f1e0279
Areum Han <hanryu1109@gmail.com>
2022-11-18 22:58:03 +0900

    feat: 퍼즐 섞기

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,11 +1,28 @@
+import { useState } from "react";
 import Tile from "../Tile/Tile";
+import { shuffle } from "../../utils/helpers";
 
 function Board({ imgUrl }) {
   const TILE_COUNT = 9;
   const GRID_SIZE = 3;
   const BOARD_SIZE = 600;
 
-  const tiles = [...Array(TILE_COUNT).keys()];
+  const [tiles, setTiles] = useState([...Array(TILE_COUNT).keys()]);
+  const [isStarted, setIsStarted] = useState(false);
+
+  const shuffleTiles = () => {
+    const shuffledTiles = shuffle(tiles);
+    setTiles(shuffledTiles);
+  };
+
+  const handleShuffleClick = () => {
+    shuffleTiles();
+  };
+
+  const handleStartClick = () => {
+    shuffleTiles();
+    setIsStarted(true);
+  };
 
   const pieceWidth = Math.round(BOARD_SIZE / GRID_SIZE);
   const pieceHeight = Math.round(BOARD_SIZE / GRID_SIZE);
@@ -28,6 +45,12 @@ function Board({ imgUrl }) {
           />
         ))}
       </ul>
+
+      {!isStarted ? (
+        <button onClick={() => handleStartClick()}>START GAME</button>
+      ) : (
+        <button onClick={() => handleShuffleClick()}>RESTART GAME</button>
+      )}
     </>
   );
 }
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,27 @@
+const TILE_COUNT = 9;
 const GRID_SIZE = 3;
+
+function isSolvable(tiles) {
+  let product = 1;
+
+  for (let i = 1, l = TILE_COUNT - 1; i <= l; i++) {
+    for (let j = i + 1, m = l + 1; j <= m; j++) {
+      product *= (tiles[i - 1] - tiles[j - 1]) / (i - j);
+    }
+  }
+
+  return Math.round(product) === 1;
+}
+
+export function isSolved(tiles) {
+  for (let i = 0, l = tiles.length; i < l; i++) {
+    if (tiles[i] !== i) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 export function getMatrixPosition(index) {
   return {
@@ -12,4 +35,17 @@ export function getVisualPosition(row, col, width, height) {
     x: col * width,
     y: row * height,
   };
+}
+
+export function shuffle(tiles) {
+  const shuffledTiles = [
+    ...tiles
+      .filter((t) => t !== tiles.length - 1)
+      .sort(() => Math.random() - 0.5),
+    tiles.length - 1,
+  ];
+
+  return isSolvable(shuffledTiles) && !isSolved(shuffledTiles)
+    ? shuffledTiles
+    : shuffle(shuffledTiles);
 }
