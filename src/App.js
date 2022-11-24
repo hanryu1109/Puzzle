3add68e4f0117ed2d2df974c8fbc538e55a5b8d1
Areum Han <hanryu1109@gmail.com>
2022-11-24 16:50:04 +0900

    feat: userpath, shortestpath 경로 저장을 위한 context 설정

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
-import Compare from "./views/Compare";
-import Game from "./views/Game";
+import { PathContext } from "./context/PathContext";
+
 import NotFound from "./views/NotFound";
 import Main from "./views/Main";
 import Menu from "./views/Menu";
+import Play from "./views/Play";
+import Compare from "./views/Compare";
 
-const App = () => (
-  <Routes>
-    <Route index element={<Main />} />
-    <Route path="/menu" element={<Menu />} />
-    <Route path="/menu/:menu_id/game" element={<Game />} />
-    <Route path="/menu/:menu_id/compare" element={<Compare />} />
-    <Route path="*" element={<NotFound />} />
-  </Routes>
-);
+const App = () => {
+  const [path1, setPath1] = useState([]);
+  const [path2, setPath2] = useState([]);
+
+  return (
+    <PathContext.Provider value={{ path1, path2, setPath1, setPath2 }}>
+      <Routes>
+        <Route index element={<Main />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/menu/:menu_id/game" element={<Play />} />
+        <Route path="/menu/:menu_id/compare" element={<Compare />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </PathContext.Provider>
+  );
+};
 export default App;
